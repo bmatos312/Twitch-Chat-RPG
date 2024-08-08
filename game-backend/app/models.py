fbbbcd45d17f3ba5f625c4349e2b60eb1cfbe7ed
bmatos312 <matos.brenden@gmail.com>
2024-08-08 14:25:23 -0400

    2: Revision of Models for bug fixes 8/8/24 2:25

--- game-backend/app/models.py
+++ game-backend/app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask_login import UserMixin
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
