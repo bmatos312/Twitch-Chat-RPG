abf37c33d74c26946b6683288abb2b7df1e7f39a
bmatos312 <142250984+bmatos312@users.noreply.github.com>
2024-09-02 11:02:09 -0400

    Delete game-backend directory

--- game-backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///game.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
