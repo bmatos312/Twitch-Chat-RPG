f8ea180654b0d8653118840bb95c9b926e643d92
bmatos312 <142250984+bmatos312@users.noreply.github.com>
2024-09-02 11:05:56 -0400

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -3,8 +3,8 @@ from authlib.integrations.flask_client import OAuth
 oauth = OAuth()
 twitch = oauth.register(
     name='twitch',
-    client_id='j2ypgxohqc0wwiq3s6qiz3685hzjmb',
-    client_secret='ouhbphhlgmkob3hqnsarm7sqlzgmho',
+    client_id='CLIENT_ID',
+    client_secret='CLIENT_SECRET',
     authorize_url='https://id.twitch.tv/oauth2/authorize',
     token_url='https://id.twitch.tv/oauth2/token',
     userinfo_url='https://api.twitch.tv/helix/users',
